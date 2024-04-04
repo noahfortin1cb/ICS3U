c95bf34f1a7eeeec69018b662010f7123e16c2bc
noahfortin1cb <noah.fortin@stu.ocsb.ca>
2024-04-04 13:40:33 -0400

    1.2

---
+++ Unit01/1.2_Variables.js
@@ -0,0 +1,10 @@
+length = 10
+width = 5
+area = length * width
+console.log(area)
+console.log("Area: "+area)
+
+FName = "Santa"
+LName = "Claws"
+LName = "Clause"
+console.log("I still believe in "+FName,LName)
