8667e119d9a38b8bf56f10771f53060d2b24c9d9
noahfortin1cb <noah.fortin@stu.ocsb.ca>
2024-04-04 13:26:12 -0400

    loard

---
+++ Unit01/1.1_Output.js
@@ -0,0 +1,11 @@
+console.log("Apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010")
+console.log("")
+console.log("")
+console.log("Microsoft Corporation \nOne Microsoft Way \nRedmond, WA \n98052 \n+1 425-882-8080")
+console.log("")
+console.log("")
+console.log("x o x\no x o\nx o x")
