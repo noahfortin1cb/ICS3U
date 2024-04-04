77f8fdc7c9636b895f2422b1a6a25519d0d3db36
noahfortin1cb <noah.fortin@stu.ocsb.ca>
2024-04-04 14:07:21 -0400

    1.3

---
+++ Unit01/1.3_Math.js
@@ -0,0 +1,19 @@
+let celcius = 100
+let fahrenheit = 0
+celcius = celcius + 2
+fahrenheit = (celcius*1.8)+32
+console.log(fahrenheit)
+
+fahrenheit = 77
+celcius = (fahrenheit - 32)/1.8
+console.log(celcius)
+
+console.log("77 degrees Fahrenheit is "+fahrenheit+" degrees in celsius.")
+
+x1 = -4
+x2 = 6
+y1 = 5
+y2 = 0
+m = (y2 - y1)/(x2 - x1)
+
+console.log(m)
