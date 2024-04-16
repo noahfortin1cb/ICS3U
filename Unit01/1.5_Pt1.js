cc5cb052917082cfba852b4efe372abaeb41c699
Noah Fortin <noah.fortin@stu.ocsb.ca>
2024-04-16 14:26:34 -0400

    1,5 pt2

--- Unit01/1.5_Pt1.js
+++ Unit01/1.5_Pt1.js
@@ -1,26 +1,34 @@
 //1.5 Part 1:
-
-const prompt = require(sync-prompt)();
+require('../utils/utils.js')
+const prompt = require('sync-prompt')();
 
 let age = prompt("How old are you? ");
 let ageInt = parseInt(age)
 
 if (ageInt >= 50) {
-    console.log("You're " + age + " years old!? You definitely qualify for the senior discount.")
+    console.log("You're " + age + " years old!? You definitely qualify for the senior discount.");
 }
 else if (ageInt < 16) {
-    console.log("Under 16? Can't drive yet.")
+    console.log("Under 16? Can't drive yet.");
 }
 else {
-    console.log("Middle aged.")
+    console.log("Middle aged.");
 }
 
-let user_name = prompt("What is your name??? ")
-let lenUser = len(user_name)
+let user_name = prompt("What is your name??? ");
+let lenUser = len(user_name);
 
 if (user_name == "Mr.J"){
-    console.log("🐠")
+    console.log("🐠");
 }
 else if (lenUser > 6){
-    console.log("You have a long name.")
+    console.log("You have a long name.");
+}
+
+let name = prompt("How long is your name? ");
+if (lenUser == name - 1){
+    console.log("Great job knowing the length of your name.");
+}
+else{
+    console.log("Bad guess.");
 }
