ff3b5e89e71ce7de1f01eea743e4e46e219514e1
Noah Fortin <noah.fortin@stu.ocsb.ca>
2024-04-22 14:03:19 -0400

    Create 2.2.js

---
+++ Unit02/2.2.js
@@ -0,0 +1,23 @@
+function is_it_the_weekend(day) {
+    if  (day == 1 || 7){
+        console.log("yes queen")
+        return true;
+        
+    }
+    else {
+        console.log("no queen")
+        return false;
+    }
+}
+is_it_the_weekend(1)
+
+function evNineIble(value){
+    if (value % 9 == 0 && value % 2 == 0){
+        console.log("Nine")
+        return true; 
+    }
+    else{
+        console.log("Nein")
+        return false;
+    }
+}
